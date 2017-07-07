02104229ef9759b6acb6502408ec8c3580138efc
Tamas Visky <visky@macadamian.com>
2017-07-08 00:42:40 +0300

    Last try before going to bed

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, my World");
+        System.out.println("Hello, World");
     }
 
 }
