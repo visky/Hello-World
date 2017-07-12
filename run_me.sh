306a4de33c7a6acfcdb14bea7070912d8598e4b7
Tamas Visky <visky@macadamian.com>
2017-07-12 11:46:55 +0300

    Add vagrant commands to run_me script

--- run_me.sh
+++ run_me.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 echo ‘This will run some tests’
+
+vagrant up  
+vagrant rsync  
+vagrant provision
+
+vagrant ssh -c "cd /vagrant; ./build.sh"  
+result=$?
+
+vagrant suspend
+
+exit ${result}
